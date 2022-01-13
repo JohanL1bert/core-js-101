926ca53a11105b1321f0c48d46ee77d940fdd799
Johan <grimmerw968@gmail.com>
2022-01-13 18:03:12 +0200

    feat: complete number task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -175,8 +175,10 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  const a = Math.round(num / 10 ** pow);
+  const b = 10 ** pow;
+  return a * b;
 }
 
 /**
